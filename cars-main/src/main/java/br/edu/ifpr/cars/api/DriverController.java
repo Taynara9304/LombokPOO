32cf9ce4109bc5444f9b6ad24188d51b4b6c7dc2
Taynara <taynaradasilva2001@gmail.com>
2025-10-13 08:16:24 -0300

    inicio

---
+++ cars-main/src/main/java/br/edu/ifpr/cars/api/DriverController.java
@@ -0,0 +1,27 @@
+package br.edu.ifpr.cars.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.ifpr.cars.domain.Driver;
+import br.edu.ifpr.cars.domain.DriverRepository;
+
+@Service
+@RestController //indica que é um controlador rest
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE) //indica que o retorno é json
+public class DriverController {
+    @Autowired //injeção de dependencia
+    DriverRepository driverRepository; //objeto instancia do repositorio
+
+    @GetMapping("/drivers")
+    public List<Driver> listDrivers() {
+        return driverRepository.findAll();
+    }
+
+}
---
+++ cars-main/src/main/java/br/edu/ifpr/cars/domain/Driver.java
@@ -0,0 +1,12 @@
+package br.edu.ifpr.cars.domain;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Driver {
+    Long id;
+    String name;
+    LocalDate birthDate;
+
+}
